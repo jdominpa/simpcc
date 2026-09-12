8aea18b50960a33616d73fdb5db4a27160057f58
Joan Domingo Pasarin <work@jdompas.com>
2026-09-12 12:18:17 +0200

    chore(devenv): fix `LLDB_DEBUGSERVER_PATH` env var

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,9 @@
 {
-  lib,
   pkgs,
   ...
 }:
 {
   languages.c.enable = true;
   packages = [ pkgs.lldb ];
-  env.LLDB_DEBUGSERVER_PATH =
-    lib.mkIf pkgs.stdenv.hostPlatform.isDarwin "/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Resources/debugserver";
+  env.LLDB_DEBUGSERVER_PATH = "/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Resources/debugserver";
 }
